df4942f50e97a0c707c38e78798604abe3449282
sibwall <sibwall@atomicmail.io>
2026-09-01 07:55:46 +0500

    Update EntryActivity.java

--- app/src/main/java/duress/ultimate/EntryActivity.java
+++ app/src/main/java/duress/ultimate/EntryActivity.java
@@ -1,5 +1,7 @@
 package duress.ultimate;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
