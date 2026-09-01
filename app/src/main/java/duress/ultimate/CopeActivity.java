72d7bbd50fb8332e4a5886cffdda23b6c27a0fa0
sibwall <sibwall@atomicmail.io>
2026-09-01 12:54:22 +0300

    Update CopeActivity.java

--- app/src/main/java/duress/ultimate/CopeActivity.java
+++ app/src/main/java/duress/ultimate/CopeActivity.java
@@ -29,6 +29,7 @@ public class CopeActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN);  
     }
 
     private static final String PREFS = "prefs";
