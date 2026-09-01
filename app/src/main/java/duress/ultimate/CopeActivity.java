bd49740da75c8bede88b374139353da60d96c448
sibwall <sibwall@atomicmail.io>
2026-09-01 08:04:28 +0500

    Update CopeActivity.java

--- app/src/main/java/duress/ultimate/CopeActivity.java
+++ app/src/main/java/duress/ultimate/CopeActivity.java
@@ -51,10 +51,44 @@ public class CopeActivity extends Activity {
                 ? "These features are available only if you have Device Owner rights. To obtain them, you must not have accounts or third-party users on the device. If they exist, delete them or just perform a factory reset.\nThen install this app again and use the adb command to activate Device Owner:\nadb shell dpm set-device-owner duress.ultimate/.MyDeviceAdminReceiver"
                 : "Эти функции доступны только если есть права Device Owner, для того чтобы их получить у вас не должно быть аккунтов и сторонних пользователей на устройстве. Если они есть, удалите их или просто сбросьте настройки.\nЗатем установите снова это приложение и используйте adb комманду для активации Device Owner:\nadb shell dpm set-device-owner duress.ultimate/.MyDeviceAdminReceiver";
 
+        String profileText = null;
+        String universalCommand = null;
+
+        if (Build.VERSION.SDK_INT >= 31) {
+            profileText = isEn()
+                    ? "Or, if you don't want to delete accounts from the main profile, you can create a work profile (if you don't have one yet) which will be able to change device-wide policies, using the ADB command:"
+                    : "Или если вы не хотите удалять аккаунты из основного профиля, вы можете создать рабочий профиль если у вас его ещё нет который сможет менять политики всего устройства, используя ADB комманду:";
+
+            String pkg = getPackageName();
+            String admin = pkg + "/" + MyDeviceAdminReceiver.class.getName();
+
+            universalCommand =
+                    "adb(){ if [ \"$1\" = \"shell\" ]; then shift; fi; \"$@\"; }; " +
+                    "USER_ID=$(adb shell pm create-user --profileOf 0 --user-type android.os.usertype.profile.MANAGED WorkProfile | grep -o '[0-9]*$') && " +
+                    "adb shell am start-user $USER_ID && " +
+                    "adb shell pm install-existing --user $USER_ID " + pkg + " && " +
+                    "adb shell dpm set-profile-owner --user $USER_ID " + admin + " && " +
+                    "adb shell dpm mark-profile-owner-on-organization-owned-device --user $USER_ID " + admin +
+                    " && adb shell am start --user $USER_ID -n " + pkg + "/.EntryActivity";
+        }
+
         deviceOwnerDialog = new AlertDialog.Builder(this)
                 .setMessage(msg)
                 .setPositiveButton("OK", (dialog, which) -> deviceOwnerDialog = null)
                 .create();
+
+        deviceOwnerDialog.setOnShowListener(d -> {
+            TextView messageView = deviceOwnerDialog.findViewById(android.R.id.message);
+            if (messageView == null) return;
+            messageView.setTextIsSelectable(true);
+
+            if (profileText != null) {
+                messageView.append("\n\n" + profileText + "\n\n" + universalCommand);
+            }
+        });
+
+        deviceOwnerDialog.setOnDismissListener(dialog -> deviceOwnerDialog = null);
+        deviceOwnerDialog.show();
 
         Window window = deviceOwnerDialog.getWindow();
         if (window != null) {
@@ -63,14 +97,6 @@ public class CopeActivity extends Activity {
             params.x = 0;
             params.y = 0;
             window.setAttributes(params);
-        }
-
-        deviceOwnerDialog.setOnDismissListener(dialog -> deviceOwnerDialog = null);
-        deviceOwnerDialog.show();
-
-        TextView messageView = deviceOwnerDialog.findViewById(android.R.id.message);
-        if (messageView != null) {
-            messageView.setTextIsSelectable(true);
         }
     }
 
